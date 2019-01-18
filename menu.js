1b4ba9d768bef5ddbb75e89a9dc48ea41dce2b8e
FemkeBlok <45420744+FemkeBlok@users.noreply.github.com>
2019-01-18 15:19:31 +0100

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,17 @@
+function accordionMenu() {
+    var menu = document.getElementById("toggled");
+    var menuStyle = window.getComputedStyle(menu);
+    var display = menuStyle.getPropertyValue('display');
+
+    if (display === 'none') {
+        toggle.style.backgroundColor = "gray";
+        menu.style.display = "block";
+    } else {
+        toggle.style.backgroundColor = "transparent";
+        menu.style.display = "none";
+    };
+};
+
+// add event listener to menu button
+var toggle = document.getElementById("menu-toggle");
+toggle.addEventListener("click", accordionMenu, false);
